19419d99af34bca22a09d48ff5e564a71087f1cd
Saurabh Brar <saurabh.barar@gmail.com>
2024-07-17 11:49:27 +0530

    Update

--- 03-07-2024/nodejs-calculator/index.js
+++ 03-07-2024/nodejs-calculator/index.js
@@ -1,75 +1,40 @@
-const crypto = require('crypto');
 
-const args = process.argv.slice(2);
-const operation = args[0];
-
-function calculate(operation, args) {
-    let num1, num2, result;
-
+/* if(process.argv[2] == "sum"){
+    console.log(Number(process.argv[3])+Number(process.argv[4]))
+} */
+    operation = process.argv[2]
+    let num1 = Number(process.argv[3])
+    let num2 = Number(process.argv[4])
+    
     switch (operation) {
-        case 'add':
-            num1 = parseFloat(args[1]);
-            num2 = parseFloat(args[2]);
-            result = num1 + num2;
-            console.log(result);
+        case "sum":
+            console.log(num1 + num2)
             break;
-
-        case 'sub':
-            num1 = parseFloat(args[1]);
-            num2 = parseFloat(args[2]);
-            result = num1 - num2;
-            console.log(result);
+    
+        case "sub":
+            console.log(num1 - num2)
             break;
-
-        case 'mult':
-            num1 = parseFloat(args[1]);
-            num2 = parseFloat(args[2]);
-            result = num1 * num2;
-            console.log(result);
-            break;
-
-        case 'divide':
-            num1 = parseFloat(args[1]);
-            num2 = parseFloat(args[2]);
-            if (num2 === 0) {
-                console.log('Error: Division by zero');
-            } else {
-                result = num1 / num2;
-                console.log(result);
-            }
-            break;
-
-        case 'sin':
-            num1 = parseFloat(args[1]);
-            result = Math.sin(num1);
-            console.log(result);
-            break;
-
-        case 'cos':
-            num1 = parseFloat(args[1]);
-            result = Math.cos(num1);
-            console.log(result);
-            break;
-
-        case 'tan':
-            num1 = parseFloat(args[1]);
-            result = Math.tan(num1);
-            console.log(result);
-            break;
-
-        case 'random':
-            let length = parseInt(args[1]);
-            if (isNaN(length)) {
-                console.log('Provide length for random number generation.');
-            } else {
-                let randomBytes = crypto.randomBytes(length);
-                let randomNumber = parseInt(randomBytes.toString('hex'), 16);
-                console.log(randomNumber);
-            }
-            break;
-
-        default:
-            console.log('Invalid operation');
+    
+        case "mult":   
+        console.log(num1 * num2)
+        break; 
+    
+        case "divide":
+            console.log(num1 / num2)
+            break; 
+        case "sin":
+            console.log(Math.sin(num1))
+            break; 
+    
+            case "cos":
+            console.log(Math.cos(num1))
+            break; 
+    
+            case "tan":
+            console.log(Math.tan(num1))
+            break; 
+    
+    
+            default:
+                console.log(Math.random100())
     }
-}
-calculate(operation, args);
